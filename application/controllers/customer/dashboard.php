fbd505690a9e3a8304dacb4b29b8222771a7c965
EdgarEvert <edgaroscar78@gmail.com>
2021-05-19 15:33:59 +0700

    part 9

--- application/controllers/customer/dashboard.php
+++ application/controllers/customer/dashboard.php
@@ -3,8 +3,9 @@
 class Dashboard extends CI_Controller {
 
     public function index() {
+        $data['konsol'] = $this->rental_model->get_data('konsol')->result();
         $this->load->view('templates_customer/header');
-        $this->load->view('customer/dashboard');
+        $this->load->view('customer/dashboard',$data);
         $this->load->view('templates_customer/footer');
     }
 }
