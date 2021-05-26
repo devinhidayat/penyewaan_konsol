188a6286cff40aa5bcf54d49ace7c76a46da47a2
EdgarEvert <edgaroscar78@gmail.com>
2021-05-26 23:04:34 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -24,6 +24,7 @@ class Auth extends CI_Controller{
                 $this->session->set_userdata('email',$cek->email);
                 $this->session->set_userdata('role_id',$cek->role_id);
                 $this->session->set_userdata('nama_customer',$cek->nama_customer);
+                $this->session->set_userdata('id_customer',$cek->id_customer);
 
                 switch ($cek->role_id) {
                     case 1 : redirect('admin/dashboard');
