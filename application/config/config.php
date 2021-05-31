8bb931539ef77937f4b651244d18871613231b80
devinhidayat <75529875+devinhidayat@users.noreply.github.com>
2021-05-31 12:31:32 +0700

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tes/UAS/penyewaan_konsol/';
+$config['base_url'] = 'https://devinhidayat.github.io/penyewaan_konsol/';
 
 /*
 |--------------------------------------------------------------------------
